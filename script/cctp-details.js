1248b099ee8ad95f153c571dd8f075d82b9316e9
d-smith <doug.smith.mail@gmail.com>
2023-12-19 07:46:53 -0800

    Added deployment of cctp contracts and harvest of deployed addresses

--- script/cctp-details.js
+++ script/cctp-details.js
@@ -5,21 +5,27 @@ const main = async () => {
     const CCTPCTX = process.env.CCTPCTX;
     const ethDeploy = require(CCTPCTX);
 
+    var output = "";
     console.log(ethDeploy.transactions);
     messageTransmitter =
         ethDeploy.transactions.filter(t => t.contractName == "MessageTransmitter" && t.transactionType == "CREATE")
             .map(t => t.contractAddress)[0];
     console.log(`export MESSAGE_TRANSMITTER=${messageTransmitter}`);
+    output += messageTransmitter + ":";
 
     tokenMessenger =
         ethDeploy.transactions.filter(t => t.contractName == "TokenMessenger" && t.transactionType == "CREATE")
             .map(t => t.contractAddress)[0];
     console.log(`export TOKEN_MESSENGER=${tokenMessenger}`);
+    output += tokenMessenger + ":";
    
     tokenMinter =
         ethDeploy.transactions.filter(t => t.contractName == "TokenMinter" && t.transactionType == "CREATE")
             .map(t => t.contractAddress)[0];
     console.log(`export TOKEN_MINTER=${tokenMinter}`);
+    output += tokenMinter;
+
+    console.log(output);
 }
 
 main();
