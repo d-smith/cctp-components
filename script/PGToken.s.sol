7048da49bc2d11e55d671596c008e29cefe7fd65
d-smith <doug.smith.mail@gmail.com>
2023-12-25 09:59:24 -0800

    start of polygon as second chain

---
+++ script/PGToken.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/Token.sol";
+
+contract DeployScript is Script {
+    uint256 private deployerPrivateKey;
+    address private ownerAddress;
+
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("PG_DEPLOYER_KEY");
+        ownerAddress = vm.envAddress("PG_OWNER_ADDRESS");
+    }
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+        new FiddyCent(ownerAddress,ownerAddress,ownerAddress);
+        vm.stopBroadcast();
+    }
+}
---
+++ script/pgdeploy-details.js
@@ -0,0 +1,11 @@
+const ethDeploy = require("../broadcast/PGToken.s.sol/80001/run-latest.json");
+
+const main = async () => {
+    //console.log(ethDeploy.transactions);
+    fiddyAddress =
+        ethDeploy.transactions.filter(t => t.contractName == "FiddyCent")
+            .map(t => t.contractAddress)[0];
+    console.log(`${fiddyAddress}`)
+}
+
+main();
